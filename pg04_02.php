e7b636c4885b093523806c27d62a8a50427748ce
marinapsribeiro <ribeiro.marina@uni9.edu.br>
2025-09-18 14:12:06 +0000

    Aula7

---
+++ pg04_02.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pg04_02</title>
+</head>
+<body>
+    <h1>Pg04_02</h1>
+    <!-- <h2>Segunda importância</h2> -->
+    <?php
+    // phpinfo();Somente para desenvolvedor
+    ?>
+</body>
+</html>
---
+++ pg1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula programação back-end</title>
+</head>
+<body>
+    <h1>PG1</h1>
+    <?php 
+    echo "<h2>Primeira programação em php</h2><br>";
+    echo "Professor Hebert"
+    ?>
+</body>
+</html>
